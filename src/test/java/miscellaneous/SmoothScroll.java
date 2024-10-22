a78830e624a0660c537e4844e515674a176f8bf1
Mitra bhanu <andolasoft.user133@.com>
2024-10-22 15:54:19 +0530

    Smooth scroll

--- src/test/java/miscellaneous/SmoothScroll.java
+++ src/test/java/miscellaneous/SmoothScroll.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -18,7 +19,9 @@ public class SmoothScroll {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 
-		for (int i = 0; i < 10000; i++) {
+		WebElement target = driver
+				.findElement(By.xpath("(//a[@title='Terms of Use'][normalize-space()='Terms of Use'])[1]"));
+		while (target.isDisplayed()) {
 			driver.findElement(By.tagName("body")).sendKeys(Keys.DOWN);
 		}
 
