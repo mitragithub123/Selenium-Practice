9d2e44d1bf2f5525994654ea7da69d0d887c92c8
Mitra bhanu <andolasoft.user133@.com>
2024-10-22 15:32:03 +0530

    Include exclude using regular expression in xml file

---
+++ src/test/java/testng/regularExpression/TestClass1.java
@@ -0,0 +1,20 @@
+package testng.regularExpression;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	@Test
+	public void ptestOne() {
+		System.out.println("TestClass1 - testOne");
+	}
+
+	@Test
+	public void ptestTwo() {
+		System.out.println("TestClass1 - testTwo");
+	}
+
+	@Test
+	public void testThree() {
+		System.out.println("TestClass1 - testThree");
+	}
+}
---
+++ src/test/java/testng/regularExpression/TestClass2.java
@@ -0,0 +1,20 @@
+package testng.regularExpression;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	@Test
+	public void ptestA() {
+		System.out.println("TestClass2 - testA");
+	}
+
+	@Test
+	public void testB() {
+		System.out.println("TestClass2 - testB");
+	}
+
+	@Test
+	public void testC() {
+		System.out.println("TestClass2 - testC");
+	}
+}
