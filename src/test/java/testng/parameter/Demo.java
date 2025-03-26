3e9b56584c0fc81a315950a7182eb64f3bc7cd1b
Mitra Bhanu Prusty <aryaaish.2011@gmail.com>
2025-03-26 15:37:50 +0530

    Parallel testing code updated

---
+++ src/test/java/testng/parameter/Demo.java
@@ -0,0 +1,15 @@
+/**
+ * @author Mitra Bhanu
+ * @created 26-Mar-2025
+ */
+package testng.parameter;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+public void get() {
+	System.out.println("Jai sriram");
+}
+}
+
